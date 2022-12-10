f82435e6f0cd496a1271d4f714cb567e90217d72
rim-DE <rim.touhami@gmail.com>
2022-12-10 18:13:12 +0100

    add extract_date to mongodb data

--- mongoDB/ETL_MongoDB.py
+++ mongoDB/ETL_MongoDB.py
@@ -3,6 +3,8 @@ from load_aircraft_position_data_in_mongodb import LoadPositionAircraftData
 import os 
 import pprint
 from pymongo import MongoClient
+from datetime import datetime
+import time
 
 host = "localhost"
 #host = "my_mongo"
@@ -10,19 +12,24 @@ port = 27017
 user = 'admin'
 password = 'pass'
 
+
+print (time.mktime(datetime.now().timetuple()))
+
+#print(time.mktime(datetime.strptime(datetime.now(), '%Y-%m-%d %H:%M:%S').timetuple()))
+
 client = MongoClient(host= host, port=port, username=user, password=password)
 mydb = client['aircraft']    
-cursor = mydb.positions.find({'icao24':'511142'})
+cursor = mydb.positions.find({'icao24':'aa56da'})
 for doc in cursor:
     pprint.pp(doc)
      
-
 '''
 #Etraction des positions des avions
 user_name_opensky='rim-DE'
 password_opensky='bde_airlines'
 p = PositionAircraftData (user_name_opensky, password_opensky)
 p.extractPositionAircrafttData ('positions.json')
+
 
 l=LoadPositionAircraftData (host, port, user, password)
 #connect to mongodb
