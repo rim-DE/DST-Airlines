2689d32e228ec14448463e0c939054ab6c314ee7
soumaya <soumaya.khila@gmail.com>
2022-12-19 15:22:26 +0100

    add : initialisation de la table aeroport

--- mysql/init.py
+++ mysql/init.py
@@ -10,21 +10,24 @@ from remplissage_mysqldb import UpdateBase
 u = UpdateBase()
 
 
+
+# initialisation de la table compagnies aériennes
+
 #transformer le fichier csv en tuples
 with open("clean_aircraft.csv") as f:
     reader = csv.reader(f)
-    aircraft_tuples = list(tuple(line) for line in reader)
-    aircraft_tuples=aircraft_tuples[2::]
-    print(aircraft_tuples[:3])
+    compagnies_tuples = list(tuple(line) for line in reader)
+    compagnies_tuples=compagnies_tuples[2::]
+    print(compagnies_tuples[:3])
 
 
-# insérer les tuples des compagnies aériennes dans la base
+# insérer les données dans la table 
 try:
     #etablir la connexion
     mydb = u.connection() 
     mycursor = mydb.cursor()
     sql="""INSERT INTO compagnies (icao24,registration,manufacturericao,manufacturername,model,serialnumber,ownername) VALUES (%s, %s,%s, %s, %s, %s , %s) ON DUPLICATE KEY UPDATE registration=VALUES(registration),  manufacturericao=VALUES(manufacturericao), manufacturername=VALUES(manufacturername), model=VALUES(model), serialnumber=VALUES(serialnumber),ownername=VALUES(ownername)"""
-    mycursor.executemany(sql, aircraft_tuples)
+    mycursor.executemany(sql, compagnies_tuples)
     mydb.commit()
     print("table compagnies remplie!")
       
@@ -38,3 +41,30 @@ finally:
     print("connection MySQL est fermé")
 
 
+# initialisation de la table aeroport
+
+with open("airport_csv.csv") as f:
+    reader = csv.reader(f)
+    aeroport_tuples = list(tuple(line) for line in reader)
+    aeroport_tuples=aeroport_tuples[2::]
+    print(aeroport_tuples[:3])
+
+
+# insérer les données dans la table aeroport
+try:
+    #etablir la connexion
+    mydb = u.connection() 
+    mycursor = mydb.cursor()
+    sql="""INSERT IGNORE INTO aeroports (ICAO, IATA, nom, taille, pays, ville) VALUES (%s, %s,%s, %s, %s, %s)"""
+    mycursor.executemany(sql, aeroport_tuples)
+    mydb.commit()
+    print("table aeroport remplie!")
+      
+except Exception:
+    print(traceback.format_exc())
+    mydb.rollback()
+      
+finally:
+    mycursor.close()
+    mydb.close()
+    print("connection MySQL est fermé")
