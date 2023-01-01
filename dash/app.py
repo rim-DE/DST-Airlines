bf399cc70f290eb6f0c2da1e1e0b2de5e7ad8850
s.soukaina <soukain.sabri>
2023-01-02 00:10:06 +0100

    update dash front

--- dash/app.py
+++ dash/app.py
@@ -1,81 +1,37 @@
-import pprint
-
 import dash
-from dash import html
 import pandas as pd
-from dash import dcc
-from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
 import plotly.express as px
+from dash.dependencies import Input, Output
+from dash import dcc
+from dash import html, ctx
+import dash_bootstrap_components as dbc
+from dash.development.base_component import Component
+from jupyter_dash import JupyterDash
+import plotly.graph_objects as go
+from dash import dash_table
 from pymongo import MongoClient
-import fontawesome as fa
 
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(
-    __name__,
-    external_stylesheets=[dbc.themes.SKETCHY],
-)
-
+app = dash.Dash(__name__ , external_stylesheets=[dbc.themes.GRID,dbc.themes.BOOTSTRAP])
 server = app.server
 
-app.layout = dbc.Container([
-    dbc.Row([
-        dbc.Col([
-            dbc.Card([
-                dbc.CardBody([
-                    html.H6('DST Tracker'),
-                    html.Div(id='none', children=[], style={'display': 'none'})
-                ], style={'textAlign': 'center'})
-
-            ]),
-        ], width=12),
-
-        dbc.Col([
-            dbc.Card([
-                dcc.Graph(id="ploting", figure={}),
-
-            ]),
-        ], width=10),
-        dbc.Col([
-
-        ], width=1),
-
-    ], className='mb-2'),
-
-    dcc.Interval(
-        id='interval-component',
-        interval=1000 * 45,  # in milliseconds
-        n_intervals=0
-    )
-
-], fluid=True)
+host = "my_mongo"
+port = 27017
+user = 'admin'
+password = 'pass'
 
 
-# ------------------
+def find_flight_states(icao24):
+    client = MongoClient(host=host, port=port, username=user, password=password)
+    database = client['aircraft']
+    position_collection = database["positions"]
 
-@app.callback(
-    Output('ploting', 'figure'),
-    [Input('interval-component', 'n_intervals', )]
-)
-def plot_map(noned):
-    states = pd.DataFrame(get_data_from_database())
-    if states.size == 0:
-        return
-
-    fig = px.scatter_mapbox(states, lat="lat", lon="long", hover_name="origin_country", zoom=2, height=1000)
-    fig.update_layout(mapbox_style="open-street-map", margin={"r": 0, "t": 0, "l": 0, "b": 0})
-
-    # fig.update_traces(marker=dict(symbol=fa.icons['arrow-up'], size=1000, color='red', opacity=0.5))
-
-    return fig
+    data = position_collection.find({"icao24": icao24}, {"icao24": 1, "long": 1, "lat": 1}).sort("time_position", -1)
+    return list(data)
 
 
-def get_data_from_database():
-    host = "my_mongo"
-    port = 27017
-    user = 'admin'
-    password = 'pass'
-
+def get_all_flights_last_state():
     client = MongoClient(host=host, port=port, username=user, password=password)
 
     database = client['aircraft']
@@ -90,10 +46,181 @@ def get_data_from_database():
             "extract_date": last_extraction[0]["extract_date"],
             "on_ground (T/F)": False
         }))
-
     return data
 
 
-# Running the server
+states = pd.DataFrame(get_all_flights_last_state())
+CountryOriginDropdown = [
+    html.Div([
+        dcc.Dropdown(
+            id='country_origin_dropdown',
+            placeholder="Select Origin Country",
+            options=[
+                {'label': i, 'value': i} for i in states.origin_country.unique()
+            ],
+            multi=True
+        ),
+    ],
+        style={"width": "80%", 'margin-left': 0, },
+    ),
+]
+
+ClearSelectionButton =   [html.Div([dbc.Button("Clear selection", color="primary", n_clicks=1, id="clear_selection_button")])]
+
+
+
+app.layout = dbc.Container([
+     dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    html.H3('Suivi du traffic aérien'),
+                ], style={'textAlign': 'center', "color": "white"})
+
+            ], className="border-0 bg-transparent"),
+        ], width=12),],style={'margin-bottom': 20 , 'backgroundColor': '#1C4E80'}),
+
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+
+                    dbc.Row(
+                    [dbc.Col(
+                        html.Div([
+                                    dcc.Markdown("""
+                                        Origin Country:
+                                    """),                                   
+                                ],style={'margin-left': "20%",'textAlign': 'center'}), md=3 ),
+                    dbc.Col(CountryOriginDropdown, md=6),
+                    
+                    dbc.Col(ClearSelectionButton, md=3)
+                     ])
+                ],)
+
+            ], style={'margin-bottom': 20, "backgroundColor": "#F1F1F1"}, #className="border-0 bg-transparent"
+            ),
+            dbc.Card([
+                dbc.CardBody([
+                    dcc.Graph(id="map"),
+                ])],style={ "backgroundColor": "#F1F1F1"} )
+
+        ], width=7),
+
+        dbc.Col([
+
+                dbc.Card([
+                dbc.CardBody([
+
+                    dbc.Row([
+                            html.Div([
+                                    dcc.Markdown("""
+                                        Flight information:
+                                    """),                                   
+                            ]),
+                    ]),
+                    dbc.Row([
+                        dash_table.DataTable(
+                                id = 'table',
+                                style_cell={'textAlign': 'left'},
+                                style_header = {'display': 'none'},
+                        ),
+                    ])
+                ], style={'textAlign': 'center'})
+
+            ], style={'margin-bottom': 30, "backgroundColor": "#F1F1F1"}, #className="border-0 bg-transparent" ,style={ "backgroundColor": "#F1F1F1"}
+            ),
+            ], width=5),
+
+    ]),
+
+    dcc.Interval(
+                        id='interval-component',
+                        interval=1000 * 45,  # in milliseconds
+                        n_intervals=0
+                    ) 
+], style={'backgroundColor':'white'},fluid=True)
+
+      
+
+@app.callback(
+    dash.dependencies.Output('table', 'data'),
+    [dash.dependencies.Input('map', 'clickData'),
+     dash.dependencies.Input('interval-component', 'n_intervals'),
+     dash.dependencies.Input('clear_selection_button', 'n_clicks')])
+def updateTable(clic_data, interval, nb_clicks):
+    states = pd.DataFrame(get_all_flights_last_state())
+    
+    
+    dt_temp1 = states.sample()
+
+    if clic_data == None:
+        dt_temp1 = dt_temp1.drop('_id', axis=1).drop_duplicates()  
+        dt_temp1 = dt_temp1.T.reset_index().T.reset_index(drop=True)
+        return dt_temp1.T.to_dict('records')
+
+    if ctx.triggered_id == "clear_selection_button":
+        return
+
+    dt_temp1 = states.loc[states['icao24'] == clic_data["points"][0]["hovertext"]]
+    dt_temp1 = dt_temp1.drop('_id', axis=1).drop_duplicates()
+    dt_temp1 = dt_temp1.T.reset_index().T.reset_index(drop=True)
+    return dt_temp1.T.to_dict('records')
+
+
+@app.callback(
+    dash.dependencies.Output('map', 'figure'),
+    [dash.dependencies.Input('country_origin_dropdown', 'value'),
+     dash.dependencies.Input('interval-component', 'n_intervals'),
+     dash.dependencies.Input('map', 'clickData'),
+     dash.dependencies.Input('clear_selection_button', 'n_clicks')
+     ])
+def display_map(country, interval, click_data, clear_button_data):
+    trigger_id = ctx.triggered_id
+    display_mode = 'markers'
+
+    if trigger_id == 'map' and click_data is not None:
+        icao24 = click_data["points"][0]["text"]
+        data = find_flight_states(icao24)
+        df_states = pd.DataFrame(data)
+        display_mode = 'lines'
+       
+    elif trigger_id == 'country_origin_dropdown':
+        df_states = pd.DataFrame(get_all_flights_last_state())
+        df_states = df_states[df_states.origin_country.isin(country)]
+
+    else:
+        df_states = pd.DataFrame(get_all_flights_last_state())
+       
+    fig = go.Figure(data=go.Scattergeo(
+        lon=df_states['long'],
+        lat=df_states['lat'],
+        text=df_states['icao24'],
+        mode=display_mode,
+        line=dict(width=4),
+    
+    ), )
+
+    fig.update_geos(
+    resolution=110,
+    showcountries=True
+    )
+    
+    fig.update_layout(
+        height=900,
+        margin=dict(l=0, r=0, t=0, b=0),
+        clickmode='event+select',
+        #paper_bgcolor="#DCDCDC",
+         mapbox = {
+            'style': "stamen-terrain"
+         }
+    )
+    return fig
+
+
 if __name__ == "__main__":
     app.run_server(debug=True, port=8050)
+
+
+
+
