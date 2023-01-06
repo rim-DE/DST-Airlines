0893b4aa21495adaa5a92627f30773f64ea39526
Wilson M. Hounsinou <62347456+mhouns@users.noreply.github.com>
2023-01-06 13:55:48 +0100

    Delete load_dashboard.sh

--- elasticsearch/load_dashboard.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -X POST localhost:5601/api/saved_objects/_import?createNewCopies=true -H "kbn-xsrf: true" --form file=@dashboard_dst_airlines_2.0.ndjson -H 'kbn-xsrf: true'
-curl -X POST localhost:5601/api/saved_objects/_import?createNewCopies=true -H "kbn-xsrf: true" --form file=@dashboard_dst_airlines_2.1.ndjson -H 'kbn-xsrf: true'
